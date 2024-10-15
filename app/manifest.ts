22d61a64415898f79bded9f8d8db6d2a9d807ae8
lowbrain <83059975+lowbrain@users.noreply.github.com>
2024-10-15 10:44:20 +0000

    modify manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,13 +14,11 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icon/web-app-manifest-192x192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: "maskable",
       },
       {
         src: '/icon/web-app-manifest-512x512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: "maskable",
       },
     ],
   }
